1ddbde723f04fe0cccb97d03c259b93e10e7f7d1
yaito3014 <ykakeyama3014@gmail.com>
2026-02-09 18:40:35 +0900

    Fix typo

--- scripts/generate_tuple_members.bat
+++ scripts/generate_tuple_members.bat
@@ -1,6 +1,6 @@
 @echo off
 cl /TP /std:c++23preview /Iinclude /I..\preprocessor\include /Imodules\iris\include /P /EP /C /DIRIS_ALLOY_GENERATE_PREPROCESSED /Fiinclude\iris\alloy\detail\preprocessed\temp.hpp include\iris\alloy\detail\tuple_impl.hpp
 pushd include\iris\alloy\detail\preprocessed
-type tuple_impl.hpp.pre.in temp.hpp tuple_impl.hpp.post.in > tuple_impl.hppp
+type tuple_impl.hpp.pre.in temp.hpp tuple_impl.hpp.post.in > tuple_impl.hpp
 del temp.hpp
 popd
