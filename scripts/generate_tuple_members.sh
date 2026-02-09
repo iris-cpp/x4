18333a67b8c80f7139f5a7d5794cdd414a1ed9e2
yaito3014 <ykakeyama3014@gmail.com>
2026-02-09 18:13:41 +0900

    Refactor preprocessing

--- scripts/generate_tuple_members.sh
+++ scripts/generate_tuple_members.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 g++ -Iinclude -I../preprocessor/include -Imodules/iris/include -E -P -DIRIS_ALLOY_GENERATE_PREPROCESSED include/iris/alloy/detail/tuple_impl.hpp > include/iris/alloy/detail/preprocessed/temp.hpp
 cd include/iris/alloy/detail/preprocessed
-sed -e '/\/\/ replace me/{r temp.hpp' -e 'd}' tuple_impl.hpp.in > tuple_impl.hpp
+cat tuple_impl.hpp.pre.in temp.hpp tuple_impl.hpp.post.in > tuple_impl.hpp
 clang-format -i tuple_impl.hpp
 rm temp.hpp
