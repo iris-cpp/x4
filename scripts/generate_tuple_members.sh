36d534ed32bc4c76a915b4a5593a58bb13360f48
yaito3014 <ykakeyama3014@gmail.com>
2026-02-09 16:58:12 +0900

    Fix include

--- scripts/generate_tuple_members.sh
+++ scripts/generate_tuple_members.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-g++ -Iinclude -Imodules/iris/include -E -P -DIRIS_ALLOY_GENERATE_PREPROCESSED include/iris/alloy/detail/tuple_impl.hpp > include/iris/alloy/detail/preprocessed/temp.hpp
+g++ -Iinclude -I../preprocessor/include -Imodules/iris/include -E -P -DIRIS_ALLOY_GENERATE_PREPROCESSED include/iris/alloy/detail/tuple_impl.hpp > include/iris/alloy/detail/preprocessed/temp.hpp
 cd include/iris/alloy/detail/preprocessed
 sed -e '/\/\/ replace me/{r temp.hpp' -e 'd}' tuple_impl.hpp.in > tuple_impl.hpp
 clang-format -i tuple_impl.hpp
