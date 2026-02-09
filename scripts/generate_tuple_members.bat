fbfd699b5462a24b664f11cc89d4a1332d73664b
yaito3014 <ykakeyama3014@gmail.com>
2026-02-09 18:32:30 +0900

    Fix

--- scripts/generate_tuple_members.bat
+++ scripts/generate_tuple_members.bat
@@ -1,4 +1,4 @@
-cl /I:include -I:..\preprocessor\include /I:modules\iris\include /P /EP /C include\iris\alloy\detail\tuple_impl.hpp /OUT:include\iris\alloy\detail\preprocessed\temp.hpp
+cl /TP /std:c++23preview /Iinclude /I..\preprocessor\include /Imodules\iris\include /P /EP /C /DIRIS_ALLOY_GENERATE_PREPROCESSED /Fiinclude\iris\alloy\detail\preprocessed\temp.hpp include\iris\alloy\detail\tuple_impl.hpp
 cd include\iris\alloy\detail\preprocessed
-type tuple_impl.hpp.pre.in temp.hpp tuple_impl.hpp.post.in
+type tuple_impl.hpp.pre.in temp.hpp tuple_impl.hpp.post.in > tuple_impl.hppp
 del temp.hpp
