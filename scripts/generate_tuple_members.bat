02917d62a31815ea335aaeab15446956845ab808
yaito3014 <ykakeyama3014@gmail.com>
2026-02-09 18:34:53 +0900

    Use pushd/popd

--- scripts/generate_tuple_members.bat
+++ scripts/generate_tuple_members.bat
@@ -1,4 +1,5 @@
 cl /TP /std:c++23preview /Iinclude /I..\preprocessor\include /Imodules\iris\include /P /EP /C /DIRIS_ALLOY_GENERATE_PREPROCESSED /Fiinclude\iris\alloy\detail\preprocessed\temp.hpp include\iris\alloy\detail\tuple_impl.hpp
-cd include\iris\alloy\detail\preprocessed
+pushd include\iris\alloy\detail\preprocessed
 type tuple_impl.hpp.pre.in temp.hpp tuple_impl.hpp.post.in > tuple_impl.hppp
 del temp.hpp
+popd
