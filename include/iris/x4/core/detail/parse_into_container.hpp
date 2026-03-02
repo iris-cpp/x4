7ac31fea8277b3f6befb9062038f27918f0c4643
yaito3014 <ykakeyama3014@gmail.com>
2026-03-02 20:05:30 +0900

    Use `handles_container`

--- include/iris/x4/core/detail/parse_into_container.hpp
+++ include/iris/x4/core/detail/parse_into_container.hpp
@@ -39,7 +39,10 @@ struct parser_accepts_container {};
 
 template<class Parser, traits::X4Container Container>
 struct parser_accepts_container<Parser, Container>
-    : traits::can_hold<typename parser_traits<Parser>::attribute_type, Container>
+    : std::disjunction<
+        traits::can_hold<typename parser_traits<Parser>::attribute_type, Container>,
+        std::bool_constant<parser_traits<Parser>::handles_container>
+    >
 {};
 
 template<class Subject, traits::X4Container Container>
