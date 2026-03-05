ff7d897e371eb15b7c9aa21d108f0f73cf25b224
yaito3014 <ykakeyama3014@gmail.com>
2026-03-05 19:08:17 +0900

    Remove subrange_traits.hpp

--- include/iris/x4/traits/attribute_category.hpp
+++ include/iris/x4/traits/attribute_category.hpp
@@ -10,7 +10,6 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
 
-#include <iris/x4/traits/subrange_traits.hpp>
 #include <iris/x4/traits/variant_traits.hpp>
 #include <iris/x4/traits/container_traits.hpp>
 #include <iris/x4/traits/optional_traits.hpp>
@@ -106,13 +105,6 @@ template<class T>
 struct attribute_category<T>
 {
     using type = optional_attr;
-};
-
-template<class T>
-    requires is_subrange_v<std::remove_cvref_t<T>>
-struct attribute_category<T>
-{
-    using type = subrange_attr;
 };
 
 template<class T>
