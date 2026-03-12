ab7b0ef5c4300fb8de061912f88123c37282f3a7
yaito3014 <ykakeyama3014@gmail.com>
2026-03-13 01:52:01 +0900

    Use more accurate type to detect narrowing

--- include/iris/x4/core/move_to.hpp
+++ include/iris/x4/core/move_to.hpp
@@ -123,9 +123,8 @@ move_to(Source&& src, Dest& dest)
     noexcept(noexcept(dest = std::forward_like<Source>(alloy::get<0>(std::forward<Source>(src)))))
 {
     static_assert(!std::same_as<std::remove_cvref_t<Source>, Dest>, "[BUG] This call should instead resolve to the overload handling identical types");
-    using element_type = std::remove_reference_t<alloy::tuple_element_t<0, std::remove_cvref_t<Source>>>;
     static_assert(
-        detail::is_assignable_without_narrowing<Dest&, element_type>::value,
+        detail::is_assignable_without_narrowing<Dest&, decltype(std::forward_like<Source>(alloy::get<0>(std::forward<Source>(src))))>::value,
         "Narrowing conversion detected in move_to (single-element tuple-like to plain)"
     );
     dest = std::forward_like<Source>(alloy::get<0>(std::forward<Source>(src)));
@@ -139,7 +138,7 @@ move_to(Source&& src, Dest& dest)
 {
     static_assert(!std::same_as<std::remove_cvref_t<Source>, Dest>, "[BUG] This call should instead resolve to the overload handling identical types");
     static_assert(
-        detail::is_assignable_without_narrowing<Dest&, std::remove_cvref_t<Source>>::value,
+        detail::is_assignable_without_narrowing<Dest&, Source>::value,
         "Narrowing conversion detected in move_to (source to plain)"
     );
     dest = std::forward<Source>(src);
@@ -203,7 +202,7 @@ move_to(Source&& src, Dest& dest)
     static_assert(!std::same_as<std::remove_cvref_t<Source>, Dest>, "[BUG] This call should instead resolve to the overload handling identical types");
     static_assert(std::is_assignable_v<Dest&, Source>);
     static_assert(
-        detail::is_assignable_without_narrowing<typename Dest::value_type&, std::remove_cvref_t<Source>>::value,
+        detail::is_assignable_without_narrowing<typename Dest::value_type&, Source>::value,
         "Narrowing conversion detected in move_to (source to optional)"
     );
     dest = std::forward<Source>(src);
