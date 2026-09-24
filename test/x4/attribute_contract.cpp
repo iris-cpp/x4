2da7c915743adfa9917b0bd1a5ee97ea4239e70b
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2026-09-25 00:22:29 +0900

    Use macro in test

--- test/x4/attribute_contract.cpp
+++ test/x4/attribute_contract.cpp
@@ -40,6 +40,7 @@
 #include <iris/x4/operator/optional.hpp>
 #include <iris/x4/operator/plus.hpp>
 #include <iris/x4/operator/sequence.hpp>
+#include <iris/x4/operator/delimited_list.hpp>
 
 #include <iris/x4/traits/attribute_traits.hpp>
 
@@ -91,9 +92,9 @@ struct throwing_parser : x4::parser<throwing_parser<T>>
 
     template<std::forward_iterator It, std::sentinel_for<It> Se, class Context, x4::X4Attribute Attr>
     [[nodiscard]] static constexpr bool
-    parse(It&, Se const&, Context const&, Attr& attr_)
+    parse(It&, Se const&, Context const&, Attr& attr)
     {
-        if (std::addressof(attr_)) {
+        if (std::addressof(attr)) {
             throw std::runtime_error("throwing_parser");
         }
         return false;
@@ -102,35 +103,6 @@ struct throwing_parser : x4::parser<throwing_parser<T>>
 
 template<class T>
 inline constexpr throwing_parser<T> always_throw{};
-
-// Contract 1 + 2
-template<class Attr, class Parser>
-void check_success(Attr poison, std::string_view input, Parser const& p, Attr const& expected)
-{
-    Attr attr = poison;
-    REQUIRE(parse(input, p, attr));
-    CHECK(attr == expected);
-}
-
-// Contract 3
-template<class Attr, class Parser>
-void check_failure(Attr poison, std::string_view input, Parser const& p)
-{
-    Attr attr = poison;
-    REQUIRE(!parse(input, p, attr));
-    CHECK(attr == Attr{});
-}
-
-// Contract 4
-template<class Attr, class Parser>
-void check_exception(Attr poison, Parser const& p)
-{
-    Attr attr = poison;
-    REQUIRE_THROWS_AS(parse("1", p, attr), std::runtime_error);
-    if constexpr (x4::traits::is_nothrow_resettable_v<Attr>) {
-        CHECK(attr == Attr{});
-    }
-}
 
 } // anonymous
 
@@ -145,24 +117,53 @@ using x4::lit;
 using x4::eps;
 using x4::fixed_value;
 using x4::standard::alpha;
+using x4::standard::digit;
+
+// Contract 1 + 2
+#define X4_TEST_SUCCESS(poison, input, p, expected) \
+    do { \
+        auto attr = poison; \
+        REQUIRE(parse(input, p, attr)); \
+        CHECK(attr == expected); \
+    } while (false)
+
+// Contract 3
+#define X4_TEST_FAILURE(poison, input, p) \
+    do { \
+        auto attr = poison; \
+        using Attr = std::remove_cvref_t<decltype(attr)>; \
+        REQUIRE(!parse(input, p, attr)); \
+        CHECK(attr == Attr{}); \
+    } while (false)
+
+// Contract 4
+#define X4_TEST_EXCEPTION(poison, p) \
+    do { \
+        auto attr = poison; \
+        using Attr = std::remove_cvref_t<decltype(attr)>; \
+        REQUIRE_THROWS_AS(parse("1", p, attr), std::runtime_error); \
+        if constexpr (x4::traits::is_nothrow_resettable_v<Attr>) { \
+            CHECK(attr == Attr{}); \
+        } \
+    } while (false)
 
 TEST_CASE("attribute contract: prior content does not influence the result")
 {
-    check_success<int>(999, "42", int_, 42);
-    check_success<int>(999, "42", int_ | fixed_value(7), 42);
+    X4_TEST_SUCCESS(999, "42", int_, 42);
+    X4_TEST_SUCCESS(999, "42", int_ | fixed_value(7), 42);
 
-    check_success<std::string>("poison"s, "abc", +alpha, "abc"s);
-    check_success<std::vector<int>>({7, 8, 9}, "1,2,3,", *(int_ >> lit(',')), std::vector<int>{1, 2, 3});
+    X4_TEST_SUCCESS("poison"s, "abc", +alpha, "abc"s);
+    X4_TEST_SUCCESS(std::vector<int>({7, 8, 9}), "1,2,3,", *(int_ >> lit(',')), std::vector<int>({1, 2, 3}));
 
     using var_t = iris::rvariant<int, std::string>;
-    check_success<var_t>(var_t{"poison"s}, "42", int_ | +alpha, var_t{42});
-    check_success<var_t>(var_t{999}, "abc", int_ | +alpha, var_t{"abc"s});
+    X4_TEST_SUCCESS(var_t{"poison"s}, "42", int_ | +alpha, var_t{42});
+    X4_TEST_SUCCESS(var_t{999}, "abc", int_ | +alpha, var_t{"abc"s});
 
-    check_success<std::optional<int>>(std::optional<int>{999}, "42", int_, std::optional<int>{42});
-    check_success<std::optional<int>>(std::optional<int>{999}, "42", -int_, std::optional<int>{42});
+    X4_TEST_SUCCESS(std::optional<int>{999}, "42", int_, std::optional<int>{42});
+    X4_TEST_SUCCESS(std::optional<int>{999}, "42", -int_, std::optional<int>{42});
 
-    check_success<Pair>(Pair{999, "poison"s}, "42abc", int_ >> +alpha, Pair{42, "abc"s});
-    check_success<Single>(Single{999}, "42", int_, Single{42});
+    X4_TEST_SUCCESS(Pair(999, "poison"s), "42abc", int_ >> +alpha, Pair(42, "abc"s));
+    X4_TEST_SUCCESS(Single{999}, "42", int_, Single{42});
 
     {
         std::unique_ptr<int> attr = std::make_unique<int>(999);
@@ -185,17 +186,17 @@ TEST_CASE("attribute contract: prior content does not influence the result")
 
 TEST_CASE("attribute contract: a failed parse resets the attribute")
 {
-    check_failure<int>(999, "x", int_);
-    check_failure<int>(999, "x", int_ | fixed_value(7) >> eps(false));
-    check_failure<std::string>("poison"s, "123", +alpha);
-    check_failure<std::vector<int>>({7, 8, 9}, "x", +(int_ >> lit(',')));
+    X4_TEST_FAILURE(999, "x", int_);
+    X4_TEST_FAILURE(999, "x", int_ | fixed_value(7) >> eps(false));
+    X4_TEST_FAILURE("poison"s, "123", +alpha);
+    X4_TEST_FAILURE(std::vector<int>({7, 8, 9}), "x", +(int_ >> lit(',')));
 
     using var_t = iris::rvariant<int, std::string>;
-    check_failure<var_t>(var_t{"poison"s}, "-", int_ | +alpha);
+    X4_TEST_FAILURE(var_t{"poison"s}, "-", int_ | +alpha);
 
-    check_failure<std::optional<int>>(std::optional<int>{999}, "x", int_);
-    check_failure<Pair>(Pair{999, "poison"s}, "42123", int_ >> +alpha);
-    check_failure<Single>(Single{999}, "x", int_);
+    X4_TEST_FAILURE(std::optional<int>{999}, "x", int_);
+    X4_TEST_FAILURE(Pair(999, "poison"s), "42123", int_ >> +alpha);
+    X4_TEST_FAILURE(Single{999}, "x", int_);
 
     {
         std::unique_ptr<int> attr = std::make_unique<int>(999);
@@ -203,7 +204,7 @@ TEST_CASE("attribute contract: a failed parse resets the attribute")
         CHECK(attr == nullptr);
     }
 
-    check_failure<Pair>(Pair{999, "poison"s}, "42x", (int_ >> +alpha >> lit('!')) | (int_ >> +alpha >> eps(false)));
+    X4_TEST_FAILURE(Pair(999, "poison"s), "42x", (int_ >> +alpha >> lit('!')) | (int_ >> +alpha >> eps(false)));
 }
 
 TEST_CASE("attribute contract: exception")
@@ -213,14 +214,24 @@ TEST_CASE("attribute contract: exception")
     STATIC_CHECK(x4::traits::is_nothrow_resettable_v<Pair>);
     STATIC_CHECK(!x4::traits::is_nothrow_resettable_v<throwing_plain>);
 
-    check_exception<int>(999, always_throw<int>);
-    check_exception<std::string>("poison"s, always_throw<std::string>);
-    check_exception<std::vector<int>>({7, 8, 9}, always_throw<std::vector<int>>);
-    check_exception<std::optional<int>>(std::optional<int>{999}, always_throw<int>);
-    check_exception<Pair>(Pair{999, "poison"s}, always_throw<Pair>);
+    X4_TEST_EXCEPTION(999, always_throw<int>);
+    X4_TEST_EXCEPTION("poison"s, always_throw<std::string>);
+    X4_TEST_EXCEPTION(std::vector<int>({7, 8, 9}), always_throw<std::vector<int>>);
+    X4_TEST_EXCEPTION(std::optional<int>{999}, always_throw<int>);
+    X4_TEST_EXCEPTION(Pair(999, "poison"s), always_throw<Pair>);
 
     using var_t = iris::rvariant<int, std::string>;
-    check_exception<var_t>(var_t{"poison"s}, always_throw<int>);
+    X4_TEST_EXCEPTION(var_t{"poison"s}, always_throw<int>);
 
-    check_exception<throwing_plain>(throwing_plain{999}, always_throw<throwing_plain>);
+    X4_TEST_EXCEPTION(throwing_plain{999}, always_throw<throwing_plain>);
+}
+
+TEST_CASE("attribute contract: parser depending on the previous result of the subject")
+{
+    X4_TEST_SUCCESS("poison"s, "a1b", *(alpha >> digit) >> lit('b'), "a1"s);
+    X4_TEST_SUCCESS("poison"s, "a1b", +(alpha >> digit) >> lit('b'), "a1"s);
+    X4_TEST_SUCCESS("poison"s, "a1,b", ((alpha >> digit) % lit(',')) >> lit(",b"), "a1"s);
+
+    X4_TEST_SUCCESS(Pair(999, "poison"s), "42x", -(int_ >> +alpha >> lit('!')) >> lit("42x"), Pair{});
+    X4_TEST_SUCCESS(std::optional{Pair(999, "poison"s)}, "42x", -(int_ >> +alpha >> lit('!')) >> lit("42x"), std::optional<Pair>{});
 }
